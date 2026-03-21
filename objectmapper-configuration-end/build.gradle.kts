05f7c896b08c7bcb87a67a30f809d68c2b41a69f
Baeldung CI User <baeldungci@baeldung.com>
2026-03-21 15:02:15 +0000

    feat: add Gradle build to objectmapper-configuration-end

---
+++ objectmapper-configuration-end/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    `java-library`
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(libs.com.fasterxml.jackson.core.jackson.databind)
+    testImplementation(libs.org.junit.jupiter.junit.jupiter)
+    testRuntimeOnly(libs.org.junit.platform.junit.platform.launcher)
+}
+
+group = "com.baeldung"
+version = "1.0.0"
+description = "objectmapper-configuration-end"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_24
+    targetCompatibility = JavaVersion.VERSION_24
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<JavaCompile>() {
+    options.encoding = "UTF-8"
+}
